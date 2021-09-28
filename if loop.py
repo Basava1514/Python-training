28cf30a567a7ee870110e34e69468a8f9d27a7a2
Basavarajeshwari BM <Basavarajeshwari.BM@sonata-software.com>
2021-09-28 12:19:22 +0530

    if loop execution

---
+++ if loop.py
@@ -0,0 +1,10 @@
+no1 = int(input("Enter no1"))
+no2 = int(input("Enter no2"))
+if no1 < no2:
+    sum1 = no1+no2
+    print(sum1)
+elif no1 > 5:
+    sub = no1-no2
+    print(sub)
+else:
+    print("Hello")
